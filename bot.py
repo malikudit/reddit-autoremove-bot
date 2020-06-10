f9db28c9ac5c1f39bbecd4dd43fcd399605d891a
Udit Malik <51827904+malikudit@users.noreply.github.com>
2020-06-10 02:07:11 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import praw
-import time
+import prawcore
+from time import sleep
 import traceback
 
 def login():
@@ -8,19 +9,33 @@ def login():
 
 r = login()
 sub = "bloonsmemes"
+downvote_threshold = -5
+upvote_threshold = 5
 
 reply_template = 'Hey monkeys, is this joke good? Upvote this comment if its dank, downvote if not.'
 
 while True:
     try:
-        for submission in r.subreddit(sub).stream.submissions.new(limit=20):
-            if submission not in r.user.me().saved(limit=None):
+        for submission in r.subreddit(sub).new():
+            if submission not in r.user.me().saved(limit=20):
                 submission.reply(reply_template)
+                print("Posted initial comment")
                 submission.save()
-        for comment in r.user.me().comments.new(limit=20):
-            if comment.score < -5:
-                comment.submission.mod.remove(spam=False)
-        time.sleep(30)
+                print("Submission saved")
+            
+        for comment in r.user.me().comments.new(limit=None):
+            if comment.score <= downvote_threshold:
+                comment.delete()
+                print("Comment removed; post isn't dank")
+            elif comment.score >= upvote_threshold:
+                c = r.user.get_comments().next()
+                c.edit("Dank")
+        
+        print("Will resume after 30 seconds.")
+        sleep(30)
+        
     except:
         traceback.print_exc()
-        time.sleep(10)
+        print("Will resume after 10 seconds.")
+        sleep(10)
+
